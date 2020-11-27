fa7e31badfeb8420e07cd116032b7f8589d83b08
Brenner <brenner.lattin@gmail.com>
2020-11-27 14:36:20 -0700

    Initial commit

---
+++ src/main/java/gearlist/GearlistApplication.java
@@ -0,0 +1,13 @@
+package gearlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gearlist/GearlistApplicationTests.java
@@ -0,0 +1,13 @@
+package gearlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
